f8d80a86ab54c89ca24e8f4f241130f42e5c8fd0
Leopold Mols <53212@etu.he2b.be>
2021-09-30 14:11:57 +0200

    Correction small display errors #story1

--- attendance-app/resources/views/student.blade.php
+++ attendance-app/resources/views/student.blade.php
@@ -2,7 +2,7 @@
 
 @section('title', 'StudentList')
 
-<?php $students = json_decode($students_json,true); ?>
+<?php $students = json_decode($students,true); ?>
 
 @section ('content')
     <h1>Listes des étudiants</h1>
@@ -13,9 +13,9 @@
             <tr><th>id</th><th>Last name</th><th>First name</th></tr>
             @foreach($students as $student)
                 <tr>
-                    <td>$student["id"]</td>
-                    <td>$student["last_name"]</td>
-                    <td>$student["first_name]</td>
+                    <td><?=$student["id"]?></td>
+                    <td><?=$student["last_name"]?></td>
+                    <td><?=$student["first_name"]?></td>
                 </tr>
             @endforeach
         </table>
